f6d63f3c0bf53c23fe4088aee2b7076c969eac9f
Gabriel Caurin <gabrielcaurin@gmail.com>
2026-03-05 16:31:34 -0300

    feat: add future activity to invert a three-digit number

--- pythonAtividades ALL/04.03.26 - Aula 03 Teorica/main.py
+++ pythonAtividades ALL/04.03.26 - Aula 03 Teorica/main.py
@@ -35,3 +35,13 @@
 #comit = sell * 0.05
 #salfinal = sal + comitc + comit
 #print(f"Salario Base: R${sal}\nComissão por carro vendido: R${comitc}\nComissão por valor de vendas: R${comit}\nSalário final: R${salfinal}")
+
+
+
+#adiantando provavelmente uma atividade futura, ler um numero inteiro de tres digitos e retornar o numero invertido  da Aula pratica 02
+#num = int(input("Digite um numero inteiro: "))
+#num1 = num // 100
+#num2 = (num % 100) // 10
+#num3 = num % 10
+#inv = (num3 * 100) + (num2 * 10) + num1
+#print(f"{num} invertido é: {inv}")
