88a36a6512dbd60958c2be80357f0064b41023eb
Gabriel Caurin <gabrielcaurin@gmail.com>
2026-03-11 11:26:19 -0300

    feat: add ex 9

--- pythonAtividades ALL/11.03.26 - Inicio IF Aula 03 Pratica/main.py
+++ pythonAtividades ALL/11.03.26 - Inicio IF Aula 03 Pratica/main.py
@@ -84,12 +84,12 @@
 
 
 #Ler a idade de lutador, se for menor que 18, Categ juvenil. Se for maior de 18 e com peso menor que 80, Peso Médio. Se for maior de 18 e peso maior que 80, Peso Pesado.   9
-age = int(input("Digite sua idade: "))
-if age < 18:
-  print("Categoria Juvenil")
-else:
-  peso = int(input("Digite seu peso em KGs: "))
-  if peso <= 80:
-    print("Peso Médio")
-  else:
-    print("Peso Pesado")
+#age = int(input("Digite sua idade: "))
+#if age < 18:
+  #print("Categoria Juvenil")
+#else:
+  #peso = int(input("Digite seu peso em KGs: "))
+  #if peso <= 80:
+    #print("Peso Médio")
+  #else:
+    #print("Peso Pesado")
